da81bbfa970f628464022c0c1c800e5fb7dd29a8
YSW0 <105997065+YSW0@users.noreply.github.com>
2022-07-08 18:30:54 -0700

    sound files

--- run.py
+++ run.py
@@ -14,12 +14,24 @@ class GameController(object):
         self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
         self.background = None
         self.clock = pygame.time.Clock()
+        self.flagg = False
+        pygame.mixer.init()
+        self.walk_sound = pygame.mixer.Sound('001.mp3')
+        self.target_sound = pygame.mixer.Sound('003.mp3')
+        
+        
     
     def setBackground(self):
         self.background = pygame.surface.Surface(SCREENSIZE).convert()
         self.background.fill((BLACK))
+        self.bi = pygame.image.load('grass_bcg.jpg').convert()
+        self.blk = pygame.image.load('Black.jpg').convert()
+        self.bix = 0
+        self.biy = 0
+        self.background.blit(self.bi, (self.bix, self.biy))
 
     def startGame(self):
+        self.target_sound.play()
         self.f = open("record.txt", "a")
         self.setBackground()
         self.pacman = Pacman()
@@ -68,7 +80,7 @@ class GameController(object):
             self.pacman.directions = {STOP:Vector2(0, 0), UP:Vector2(0,-1), DOWN:Vector2(0,1), LEFT:Vector2(-1,0), RIGHT:Vector2(1,0)}#{STOP:np.array([0, 0]), UP:np.array([0, -1]), DOWN:np.array([0, 1]), LEFT:np.array([-1,0]), RIGHT:np.array([1,0])}#
             
        
-        if self.pacman.getValidKey() == DOWN:
+        if self.pacman.getValidKey() == DOWN and self.flagg == True:
             sprites_surf = pygame.image.load('hdhd.png').convert()
             self.pacman.image.blit(sprites_surf, (0, 0), (0, 0, 36, 42))
             self.pacman.render(self.screen)
@@ -80,7 +92,7 @@ class GameController(object):
             self.pacman.render(self.screen)
             pygame.display.update()
         
-        if self.pacman.getValidKey() == UP:
+        if self.pacman.getValidKey() == UP and self.flagg == True:
             sprites_surf = pygame.image.load('hdhd.png').convert()
             self.pacman.image.blit(sprites_surf, (0, 0), (0, 124, 36, 42))
             self.pacman.render(self.screen)
@@ -92,7 +104,7 @@ class GameController(object):
             self.pacman.render(self.screen)
             pygame.display.update()
 
-        if self.pacman.getValidKey() == LEFT:
+        if self.pacman.getValidKey() == LEFT and self.flagg == True:
             sprites_surf = pygame.image.load('hdhd.png').convert()
             self.pacman.image.blit(sprites_surf, (0, 0), (0, 43, 36, 42))
             self.pacman.render(self.screen)
@@ -104,7 +116,7 @@ class GameController(object):
             self.pacman.render(self.screen)
             pygame.display.update()
 
-        if self.pacman.getValidKey() == RIGHT:
+        if self.pacman.getValidKey() == RIGHT and self.flagg == True:
             sprites_surf = pygame.image.load('hdhd.png').convert()
             self.pacman.image.blit(sprites_surf, (0, 0), (0, 83, 36, 42))
             self.pacman.render(self.screen)
@@ -121,14 +133,22 @@ class GameController(object):
         self.target.update(dt)
 
 #self.wall.update(dt)
-        direction = self.pacman.getValidKey()
-        self.pacman.direction = direction
-        for wall in self.group:
-            wall.x -= (self.pacman.directions[self.pacman.direction].x)*(self.pacman.speed*dt)
-            wall.y -= (self.pacman.directions[self.pacman.direction].y)*(self.pacman.speed*dt)
-        self.target.position -= self.pacman.directions[self.pacman.direction] * self.pacman.speed*dt
-        
+        if self.flagg == True:
+            direction = self.pacman.getValidKey()
+            self.pacman.direction = direction
+            for wall in self.group:
+                wall.x -= (self.pacman.directions[self.pacman.direction].x)*(self.pacman.speed*dt)
+                wall.y -= (self.pacman.directions[self.pacman.direction].y)*(self.pacman.speed*dt)
+            self.target.position -= self.pacman.directions[self.pacman.direction] * self.pacman.speed*dt
 
+
+            self.background.blit(self.blk, (self.bix, self.biy))
+            self.bix -= (self.pacman.directions[self.pacman.direction].x)*(self.pacman.speed*dt)
+            self.biy -= (self.pacman.directions[self.pacman.direction].y)*(self.pacman.speed*dt)
+            self.background.blit(self.bi, (self.bix, self.biy))
+            
+            
+        
 
         self.render()
     
@@ -213,21 +233,69 @@ class GameController(object):
         for element in self.group:
             element.render(self.screen)
         #self.group.draw(self.screen)
-    
-
 
         pygame.display.update()
+    
+def word_wrap(rect, font, color, text):
 
+    size = w, h = rect.size
+    surf = pygame.Surface(size, pygame.SRCALPHA, 32)
+    surf = surf.convert_alpha()
+    x = 0
+    y = 0
+    for char in text:
+        text_surface = font.render(char, True, color)
+        width, height = text_surface.get_size()
+        if x + width >= w:
+            y = y + height
+            x = 0
+        if y + height > h:
+            break
+        surf.blit(text_surface, (x, y))
+        x = x + width
+        
+    return surf
+    
+def setup_fonts(font_size, bold=False, italic=False):
+    
+
+    font_preferences = ['Helvetica Neue', 'Iosevka Regular', 'Comic Sans', 'Courier New']
+    available = pygame.font.get_fonts()
+    prefs = [x.lower().replace(' ', '') for x in font_preferences]
+    for pref in prefs:
+        a = [x
+            for x in available
+            if x.startswith(pref)
+            ]
+        if a:
+            fonts = ','.join(a) #SysFont expects a string with font names in it
+            return pygame.font.SysFont(fonts, font_size, bold, italic)
+    return pygame.font.SysFont(None, font_size, bold, italic)
         
 
 if __name__ == "__main__":
     game = GameController()
     game.startGame()
-    
-while True:
     game.update()
+
+    fontobj = setup_fonts(18)
+    wrapped_rect = pygame.Rect(100, 30, 210, 500)
+    wrapped_surface = word_wrap(wrapped_rect, fontobj, (200, 0, 0), "Press 1 to start your game")
+    game.screen.blit(wrapped_surface, wrapped_rect)
+    pygame.draw.rect(game.screen, (0,0,0), wrapped_rect, width=1)
+    pygame.display.flip()
+
+while True:
+    
+    game.update()
+    '''
+    distance = (game.target.position.x - game.pacman.position.x)*(game.target.position.x - game.pacman.position.x) + (game.target.position.y - game.pacman.position.y)*(game.target.position.y - game.pacman.position.y)
+    game.target_sound.set_volume(distance/20000)
+    game.target_sound.play()
+    '''
     lim = 260
     if pygame.key.get_pressed()[K_1]:
+        game.flagg = True
         while game.wall_1.height < lim:
             pygame.time.delay(1)
             for element in game.group:
@@ -240,6 +308,14 @@ while True:
     
     if pygame.sprite.collide_rect(game.pacman, game.target):
         break
+    if pygame.key.get_pressed()[K_UP] or pygame.key.get_pressed()[K_DOWN] or pygame.key.get_pressed()[K_LEFT] or pygame.key.get_pressed()[K_RIGHT]:
+        if game.flagg == True:
+            game.walk_sound.play()
+    else:
+        game.walk_sound.stop()
+
+        
+        
 
 game.secondScene()
 
